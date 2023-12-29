a8b74fa0f73ef3ab5c5dcb19e404bf3b32ad04b4
billonium4535 <billy.sethgrimmett@gmail.com>
2023-12-29 22:32:34 +0000

    Added sending csv file data

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socket
 import threading
+import os
+import csv
+import time
 
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8446
+SERVER_MESSAGES = "messages.csv"
 
 # List to store connected client sockets
 connected_clients = []
@@ -10,6 +14,8 @@ connected_clients = []
 
 def handle_client(client_socket):
     try:
+        # Send the messages.csv file to the client
+        send_file(client_socket, SERVER_MESSAGES)
         while True:
             # Receive data from the client
             data = client_socket.recv(1024)
@@ -17,10 +23,17 @@ def handle_client(client_socket):
                 break
 
             # Print the received message
-            print(f"Received message from {client_socket.getpeername()}: {data.decode('utf-8')}")
+            print(f"Received message from {client_socket.getpeername()[0]}: {data.decode('windows-1252')}")
+
+            with open(SERVER_MESSAGES, 'a', newline='', encoding="windows-1252") as csv_file:
+                # Create a CSV writer object
+                csv_writer = csv.writer(csv_file)
+
+                # Write the data to a new line in the CSV file
+                csv_writer.writerow([f"{time.strftime('%H:%M:%S', time.localtime())} : {client_socket.getpeername()[0]} : {data.decode('windows-1252')}"])
 
             # Broadcast the message to all connected clients except the sender
-            broadcast_message(data, client_socket)
+            broadcast_message(f"{time.strftime('%H:%M:%S', time.localtime())} : {client_socket.getpeername()[0]} : {data.decode('windows-1252')}", client_socket)
 
     except ConnectionResetError:
         print(f"Connection with {client_socket.getpeername()} closed by client.")
@@ -32,6 +45,19 @@ def handle_client(client_socket):
         client_socket.close()
 
 
+def send_file(client_socket, file_name):
+    try:
+        with open(file_name, 'rb') as file:
+            data = file.read(1024)
+            while data:
+                client_socket.send(data)
+                data = file.read(1024)
+    except FileNotFoundError:
+        print(f"File {file_name} not found.")
+    except Exception as e:
+        print(f"Error sending file {file_name}: {e}")
+
+
 def broadcast_message(message, sender_socket):
     # Iterate through the list of connected clients and send the message to each client
     for client in connected_clients:
@@ -39,7 +65,7 @@ def broadcast_message(message, sender_socket):
         # if client != sender_socket:
         try:
             # Send the message to the client
-            client.send(message)
+            client.send(message.encode('windows-1252'))
         except socket.error:
             # Remove the client socket from the list if there is an error sending the message
             connected_clients.remove(client)
@@ -52,8 +78,8 @@ def start_server():
     # Bind the socket to a specific address and port
     server_socket.bind((SERVER_HOST, SERVER_PORT))
 
-    # Listen for incoming connections (maximum of 5)
-    server_socket.listen(5)
+    # Listen for incoming connections (maximum of 10)
+    server_socket.listen(10)
     print(f"Server listening on {SERVER_HOST}:{SERVER_PORT}")
 
     while True:
