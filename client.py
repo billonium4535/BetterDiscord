b303f1dd3259756e835b2a47b7136720c6fac29e
billonium4535 <billy.sethgrimmett@gmail.com>
2023-12-29 17:14:18 +0000

    Modified for testing

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 import socket
 
-SERVER_HOST = '192.160.0.86'
+SERVER_HOST = '82.20.26.36'
 SERVER_PORT = 8446
 
 
 def start_client():
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print(f"Attempting to connect to {SERVER_HOST}:{SERVER_PORT}")
 
     # Connect to the server
     client_socket.connect((SERVER_HOST, SERVER_PORT))
