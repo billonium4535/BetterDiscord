bf343594e653c1e0835d2cd23944b5a0c9768890
billonium4535 <billy.sethgrimmett@gmail.com>
2023-12-29 11:49:34 +0000

    Allowed for sending messages

--- client.py
+++ client.py
@@ -1,7 +1,28 @@
 import socket
-import threading
-import sounddevice
-import numpy
 
-SERVER_HOST = ''
-SERVER_PORT = 00000
+SERVER_HOST = '192.160.0.86'
+SERVER_PORT = 8446
+
+
+def start_client():
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+    while True:
+        # Get user input and send it to the server
+        message = input("Enter a message (or 'exit' to quit): ")
+        if message.lower() == 'exit':
+            break
+
+        # Send the message to the server
+        client_socket.send(message.encode('utf-8'))
+
+    # Close the client socket when done
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    start_client()
--- server.py
+++ server.py
@@ -1,7 +1,44 @@
 import socket
 import threading
-import sounddevice
-import numpy
 
-SERVER_HOST = ''
-SERVER_PORT = 00000
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8446
+
+
+def handle_client(client_socket):
+    while True:
+        # Receive data from the client
+        data = client_socket.recv(1024)
+        if not data:
+            break
+
+        # Print the received message
+        print(f"Received message from {client_socket.getpeername()}: {data.decode('utf-8')}")
+
+    # Close the client socket when the connection is terminated
+    client_socket.close()
+
+
+def start_server():
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to a specific address and port
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+    # Listen for incoming connections (maximum of 5)
+    server_socket.listen(5)
+    print(f"Server listening on {SERVER_HOST}:{SERVER_PORT}")
+
+    while True:
+        # Accept a connection from a client
+        client_socket, addr = server_socket.accept()
+        print(f"Accepted connection from {addr}")
+
+        # Start a new thread to handle the client
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+
+
+if __name__ == "__main__":
+    start_server()
