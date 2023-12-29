b2027f80a5ff4f1ea555ebc80fae7e925a95060e
billonium4535 <billy.sethgrimmett@gmail.com>
2023-12-29 17:28:19 +0000

    Added client messaging

--- client.py
+++ client.py
@@ -1,7 +1,20 @@
 import socket
+import threading
 
 SERVER_HOST = '82.20.26.36'
 SERVER_PORT = 8446
+
+
+def receive_messages(client_socket):
+    try:
+        while True:
+            # Receive and print messages from the server
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(f"Received message: {data.decode('utf-8')}")
+    except ConnectionResetError:
+        print("Connection with the server closed.")
 
 
 def start_client():
@@ -13,17 +26,26 @@ def start_client():
     # Connect to the server
     client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-    while True:
-        # Get user input and send it to the server
-        message = input("Enter a message (or 'exit' to quit): ")
-        if message.lower() == 'exit':
-            break
+    # Start a separate thread for receiving messages
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.start()
 
-        # Send the message to the server
-        client_socket.send(message.encode('utf-8'))
+    try:
+        while True:
+            # Get user input and send it to the server
+            message = input("Enter a message (or 'exit' to quit): ")
+            if message.lower() == 'exit':
+                break
 
-    # Close the client socket when done
-    client_socket.close()
+            # Send the message to the server
+            client_socket.send(message.encode('utf-8'))
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Close the client socket when done
+        client_socket.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -4,19 +4,44 @@ import threading
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8446
 
+# List to store connected client sockets
+connected_clients = []
+
 
 def handle_client(client_socket):
-    while True:
-        # Receive data from the client
-        data = client_socket.recv(1024)
-        if not data:
-            break
+    try:
+        while True:
+            # Receive data from the client
+            data = client_socket.recv(1024)
+            if not data:
+                break
 
-        # Print the received message
-        print(f"Received message from {client_socket.getpeername()}: {data.decode('utf-8')}")
+            # Print the received message
+            print(f"Received message from {client_socket.getpeername()}: {data.decode('utf-8')}")
 
-    # Close the client socket when the connection is terminated
-    client_socket.close()
+            # Broadcast the message to all connected clients except the sender
+            broadcast_message(data, client_socket)
+
+    except ConnectionResetError:
+        # Handle the case when the client closes the connection
+        print(f"Connection with {client_socket.getpeername()} closed by client.")
+    finally:
+        # Remove the client socket from the list when the connection is terminated
+        connected_clients.remove(client_socket)
+        client_socket.close()
+
+
+def broadcast_message(message, sender_socket):
+    # Iterate through the list of connected clients and send the message to each client
+    for client in connected_clients:
+        # Check if the client is not the sender
+        if client != sender_socket:
+            try:
+                # Send the message to the client
+                client.send(message)
+            except socket.error:
+                # Remove the client socket from the list if there is an error sending the message
+                connected_clients.remove(client)
 
 
 def start_server():
@@ -34,6 +59,9 @@ def start_server():
         # Accept a connection from a client
         client_socket, addr = server_socket.accept()
         print(f"Accepted connection from {addr}")
+
+        # Add the client socket to the list of connected clients
+        connected_clients.append(client_socket)
 
         # Start a new thread to handle the client
         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
