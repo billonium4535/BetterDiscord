faca44ef032805b033f3f1fd21bcdf3f027faabc
BugBuster27 <bgrimmett@pektron.co.uk>
2024-02-02 08:35:30 +0000

    Updated colors

--- Client/updater.py
+++ Client/updater.py
@@ -8,14 +8,7 @@ import ipaddress
 
 colors = {
     "white": (255, 255, 255),
-    "black": (0, 0, 0),
-    "red": (255, 0, 0),
-    "grey": (128, 128, 128),
-    "discord-dark": (49, 51, 56),
-    "discord-panel": (43, 45, 49),
-    "discord-message-box": (56, 58, 64),
-    "discord-divider": (38, 40, 44),
-    "discord-text": (242, 243, 245)
+    "discord-dark": (49, 51, 56)
 }
 
 
