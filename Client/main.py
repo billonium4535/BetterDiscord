42f61020a98f10b4763b9c121d831b8397e46732
BugBuster27 <bgrimmett@pektron.co.uk>
2024-02-02 10:17:15 +0000

    Modified to run exe files if they exist.

--- Client/main.py
+++ Client/main.py
@@ -1,6 +1,7 @@
 import pygame
 import subprocess
 import os
+import time
 
 from GUI import MainWindowGUI
 from updater import UpdaterWindowGUI
@@ -23,12 +24,30 @@ def check_client_exit():
     return client_quit
 
 
+def check_natural_client_exit():
+    if os.path.exists("./CLIENT_NATURAL_QUIT"):
+        client_quit = True
+        os.remove("./CLIENT_NATURAL_QUIT")
+    else:
+        client_quit = False
+
+    return client_quit
+
+
 if __name__ == "__main__":
     pygame.init()
     client_init()
     if os.path.exists("./updater.exe"):
         subprocess.run("./updater.exe", check=True)
+        while not check_client_exit() or not check_natural_client_exit():
+            time.sleep(3)
+        if os.path.exists("./GUI.exe"):
+            subprocess.run("./GUI.exe", check=True)
+        else:
+            MainWindowGUI()
     else:
         UpdaterWindowGUI()
-        if not check_client_exit():
+        if os.path.exists("./GUI.exe"):
+            subprocess.run("./GUI.exe", check=True)
+        else:
             MainWindowGUI()
