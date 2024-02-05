2960d17309ed153e2fb876bb7c04160163ae18a7
BugBuster27 <bgrimmett@pektron.co.uk>
2024-02-05 13:00:26 +0000

    Added error handling

--- Installer.py
+++ Installer.py
@@ -4,13 +4,18 @@ import zipfile
 
 
 def download_latest_version():
-    response = requests.get("http://127.0.0.1:8459")
-    if response.status_code == 200:
-        with open("BetterDiscord.zip", "wb") as file:
-            file.write(response.content)
-        print("Downloaded")
-    else:
-        print("Failed")
+    try:
+        response = requests.get("http://127.0.0.1:8459")
+        if response.status_code == 200:
+            with open("BetterDiscord.zip", "wb") as file:
+                file.write(response.content)
+            print("Downloaded")
+        else:
+            print("Failed")
+    except requests.exceptions.ConnectionError:
+        print("Server not available")
+    except FileNotFoundError:
+        print("BetterDiscord.zip does not exist")
 
 
 def extract_zip(zip_file_path, extract_to):
